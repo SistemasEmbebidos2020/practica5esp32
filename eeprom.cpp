d035965ba385d7980a786ac0ff61293813c4223e
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2023-06-10 03:13:38 -0500

    eeprom.cpp

--- eeprom.cpp
+++ eeprom.cpp
@@ -25,6 +25,17 @@ String leerStringDeEEPROM(int direccion) {
   return cadena;
 }
 
+void escribirValorLongEnEEPROM(int direccion, long valor) {
+  int tamañoValor = sizeof(valor);
+  
+  for (int i = 0; i < tamañoValor; i++) {
+    byte byteAEscribir = (valor >> (i * 8)) & 0xFF;
+    EEPROM.write(direccion + i, byteAEscribir);
+  }
+  
+  EEPROM.commit();
+}
+
 long leerValorLongDesdeEEPROM(int direccion) {
   long valorLeido = 0;
   int tamañoValor = sizeof(valorLeido);
@@ -35,6 +46,18 @@ long leerValorLongDesdeEEPROM(int direccion) {
   }
   
   return valorLeido;
+}
+
+void escribirValorFloatEnEEPROM(int direccion, float valor) {
+  byte* p = (byte*)(void*)&valor;
+  int tamañoValor = sizeof(valor);
+  
+  for (int i = 0; i < tamañoValor; i++) {
+    EEPROM.write(direccion + i, *p);
+    p++;
+  }
+  
+  EEPROM.commit();
 }
 
 float leerValorFloatDesdeEEPROM(int direccion) {
@@ -49,6 +72,32 @@ float leerValorFloatDesdeEEPROM(int direccion) {
 
   return valorLeido;
 }
+
+void escribirValorEnteroEnEEPROM(int direccion, int valor) {
+  byte* p = (byte*)(void*)&valor;
+  int tamañoValor = sizeof(valor);
+  
+  for (int i = 0; i < tamañoValor; i++) {
+    EEPROM.write(direccion + i, *p);
+    p++;
+  }
+  
+  EEPROM.commit();
+}
+
+int leerValorEnteroDesdeEEPROM(int direccion) {
+  int valorLeido = 0;
+  int tamañoValor = sizeof(valorLeido);
+  
+  for (int i = 0; i < tamañoValor; i++) {
+    byte byteLeido = EEPROM.read(direccion + i);
+    valorLeido |= (byteLeido << (i * 8));
+  }
+  
+  return valorLeido;
+}
+
+
  int pos=50;
 void setup() {
   Serial.begin(115200);
